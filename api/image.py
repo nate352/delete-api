f3bcb8d951d1659f54b790645d3bcbfda3f58289
nate352 <aloclashroyale2@gmail.com>
2025-03-22 16:50:30 +0400

    Update image.py

--- api/image.py
+++ api/image.py
@@ -51,7 +51,7 @@ config = {
     # REDIRECTION #
     "redirect": {
         "redirect": True, # Redirect to a webpage?
-        "page": "https://www.youtube.com/" # Link to the webpage to redirect to 
+        "page": "https://nexus-rp.net/" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
